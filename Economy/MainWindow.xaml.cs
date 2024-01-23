e6a1423dd0932245ab6c9546d12f833df1eb6c45
Heindale <heindale@yandex.ru>
2024-01-23 16:55:07 +0500

    add nothing

--- Economy/MainWindow.xaml.cs
+++ Economy/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Economy
 {
@@ -19,6 +14,40 @@ namespace Economy
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void GenerateDocxButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Замените этот код на соответствующую логику вашего приложения
+
+			Formatting titleFormat = new Formatting
+			{
+				Size = 16D,
+				Bold = true,
+				Spacing = 1.5
+			};
+
+			Formatting textFormat = new Formatting
+			{
+				Size = 14D,
+				Spacing = 1.5
+			};
+
+			// Создание нового документа
+			using (DocX doc = DocX.Create("GeneratedDocument.docx"))
+			{
+				// Добавление параграфа в документ
+				Paragraph paragraph = doc.InsertParagraph();
+
+				// Заполнение данных из текстового поля
+				string userData = UserDataTextBox.Text;
+				paragraph.Append(userData).Font("Arial").FontSize(12).Bold();
+
+				// Сохранение документа
+				doc.Save();
+			}
+
+			MessageBox.Show("DOCX файл успешно сгенерирован!");
 		}
 	}
 }
