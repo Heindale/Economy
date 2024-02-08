083377e808a1919d349c208e0e648fc938ad1be7
Heindale <heindale@yandex.ru>
2024-02-08 13:11:33 +0500

    update LoadWindow

--- Economy/MainWindow.xaml.cs
+++ Economy/MainWindow.xaml.cs
@@ -10,302 +10,318 @@ using Xceed.Words.NET;
 
 namespace Economy
 {
-    public partial class MainWindow : Window
-    {
-        private List<TextBoxData> textBoxDataList = new List<TextBoxData>();
+	public partial class MainWindow : Window
+	{
+		private List<TextBoxData> textBoxDataList = new List<TextBoxData>();
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.title = new Title();
-            this.page1 = new Chapter1();
-            this.page2 = new Chapter2();
-            this.page3 = new Chapter3();
-            this.page4 = new Chapter4();
-            this.page5 = new Chapter5();
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.title = new Title();
+			this.page1 = new Chapter1();
+			this.page2 = new Chapter2();
+			this.page3 = new Chapter3();
+			this.page4 = new Chapter4();
+			this.page5 = new Chapter5();
+		}
 
-        }
+		public DocX document { get; set; }
+		public int MyProperty { get; set; }
+		public Title title { get; set; }
+		public Chapter1 page1 { get; set; }
+		public Chapter2 page2 { get; set; }
+		public Chapter3 page3 { get; set; }
+		public Chapter4 page4 { get; set; }
+		public Chapter5 page5 { get; set; }
+		public string LastLoadFile;
 
-        public DocX document { get; set; }
-        public int MyProperty { get; set; }
-        public Title title { get; set; }
-        public Chapter1 page1 { get; set; }
-        public Chapter2 page2 { get; set; }
-        public Chapter3 page3 { get; set; }
-        public Chapter4 page4 { get; set; }
-        public Chapter5 page5 { get; set; }
-        public string LastLoadFile;
+		private static void ReplacePlaceholder(DocX document, string placeholder, string value)
+		{
+			foreach (var paragraph in document.Paragraphs)
+			{
+				if (paragraph.Text.Contains(placeholder))
+				{
+					paragraph.ReplaceText(placeholder, value);
+				}
+			}
+		}
 
-        private static void ReplacePlaceholder(DocX document, string placeholder, string value)
-        {
-            foreach (var paragraph in document.Paragraphs)
-            {
-                if (paragraph.Text.Contains(placeholder))
-                {
-                    paragraph.ReplaceText(placeholder, value);
-                }
-            }
-        }
+		public void ChangePage1()
+		{
+			SetText(page1.Page1);
+		}
 
+		public void ChangePage2()
+		{
+			SetText(page2.Page2);
+		}
 
-        private void ChangePage1()
-        {
-            SetText(page1.Page1);
-        }
+		public void ChangePage3()
+		{
+			SetText(page3.Page3);
+		}
 
-        private void ChangePage2()
-        {
-            SetText(page2.Page2);
-        }
+		public void ChangePage4()
+		{
+			SetText(page4.Page4);
+		}
 
-        private void ChangePage3()
-        {
-            SetText(page3.Page3);
-        }
+		public void ChangePage5()
+		{
+			SetText(page5.Page5);
+		}
 
-        private void ChangePage4()
-        {
-            SetText(page4.Page4);
-        }
-        private void ChangePage5()
-        {
-            SetText(page5.Page5);
-        }
-        private void Title_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.title);
-        }
+		private void Title_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.title);
+		}
 
-        private void Chapter1_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.page1);
-        }
+		private void Chapter1_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.page1);
+		}
 
-        private void Chapter2_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.page2);
-        }
+		private void Chapter2_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.page2);
+		}
 
-        private void Chapter3_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.page3);
-        }
+		private void Chapter3_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.page3);
+		}
 
-        private void Chapter4_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.page4);
-        }
-        private void Chapter5_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentPage.Navigate(this.page5);
-        }
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+		private void Chapter4_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.page4);
+		}
 
-        private IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
-        {
-            if (depObj != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-                {
-                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+		private void Chapter5_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage.Navigate(this.page5);
+		}
 
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
 
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-        }
-        private async void GenerateDocxButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.document = DocX.Load("..\\..\\..\\Resources\\TemplateVersionSecond.docx");
-            SaveFileDialog folderBrowser = new SaveFileDialog();
-            folderBrowser.Filter = "Word documents (*.docx)|*.docx";
-            if (folderBrowser.ShowDialog() == true)
-            {
-                ChangePage1();
-                ChangePage2();
-                ChangePage3();
-                ChangePage4();
-                ChangePage5();
-                try
-                {
-                    this.document.SaveAs(folderBrowser.FileName);
-                    MessageBox.Show("DOCX файл успешно сгенерирован!");
-                }
-                catch
-                {
-                    MessageBox.Show("Возможно у вас открыт документ. Проверьте ни открыт ли у вас сохраняемый документ и закройте его.");
-                }
-            }
-        }
+		private IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+				{
+					DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
 
-        private void Load_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog folderBrowser = new OpenFileDialog();
-            folderBrowser.Filter = "XML files (*.xml)|*.xml";
-            if (folderBrowser.ShowDialog() == true)
-            {
-                LastLoadFile = folderBrowser.FileName;
-                LoadDataAllPage();
-            }
-        }
+					if (child != null && child is T)
+					{
+						yield return (T)child;
+					}
 
-        private void LoadDataAllPage()
-        {
-            LoadDataOfPage(page1.Page1, 1);
-            LoadDataOfPage(page2.Page2, 2);
-            LoadDataOfPage(page3.Page3, 3);
-            LoadDataOfPage(page4.Page4, 4);
-            LoadDataOfPage(page5.Page5, 5);
-        }
+					foreach (T childOfChild in FindVisualChildren<T>(child))
+					{
+						yield return childOfChild;
+					}
+				}
+			}
+		}
 
-        private void LoadDataOfPage(Grid xamlpage, byte t)
-        {
+		private void GenerateDocxButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.document = DocX.Load("..\\..\\..\\Resources\\TemplateVersionSecond.docx");
+			SaveFileDialog folderBrowser = new SaveFileDialog();
+			folderBrowser.Filter = "Word documents (*.docx)|*.docx";
+			if (folderBrowser.ShowDialog() == true)
+			{
+				var thr = new Thread(ShowWindow);
+				thr.SetApartmentState(ApartmentState.STA);
+				thr.IsBackground = true;
+				thr.Start();
+				ChangePage1();
+				ChangePage2();
+				ChangePage3();
+				ChangePage4();
+				ChangePage5();
+				try
+				{
+					this.document.SaveAs(folderBrowser.FileName);
+					MessageBox.Show("DOCX файл успешно сгенерирован!");
+				}
+				catch
+				{
+					MessageBox.Show("Возможно у вас открыт документ. Проверьте ни открыт ли у вас сохраняемый документ и закройте его.");
+				}
+			}
+		}
 
-            // Создайте сериализатор XML
-            XmlSerializer serializer = new XmlSerializer(typeof(List<TextBoxData>));
+		private void ShowWindow()
+		{
+			var window = new LoadWindow();
+			window.Show();
+			System.Windows.Threading.Dispatcher.Run();
+		}
 
-            // Десериализуйте коллекцию из файла
-            using (TextReader reader = new StreamReader(LastLoadFile))
-            {
-                textBoxDataList = ((List<TextBoxData>)serializer.Deserialize(reader));
-            }
-            var filteredList = textBoxDataList.Where(data => data.TextBoxName.Contains($"T{t}")).ToList();
-            // Проход по коллекции и установка текста в соответствующие TextBox
-            foreach (var textBoxData in filteredList)
-            {
-                if (textBoxData.Type == typeof(TextBox).Name)
-                {
-                    TextBox textBox = (TextBox)xamlpage.FindName(textBoxData.TextBoxName);
+		public static async Task ProgressBarStartAsync(LoadWindowVerisonSecond loadWindow)
+		{
+			await Task.Run(() => { loadWindow.ShowDialog(); });
+		}
 
-                    if (textBox != null)
-                    {
-                        textBox.Text = textBoxData.Text;
-                    }
-                }
-                else
-                {
-                    ComboBox textBox = (ComboBox)xamlpage.FindName(textBoxData.TextBoxName);
+		private void Load_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog folderBrowser = new OpenFileDialog();
+			folderBrowser.Filter = "XML files (*.xml)|*.xml";
+			if (folderBrowser.ShowDialog() == true)
+			{
+				LastLoadFile = folderBrowser.FileName;
+				LoadDataAllPage();
+			}
+		}
 
-                    if (textBox != null)
-                    {
-                        textBox.Text = textBoxData.Text;
-                    }
-                }
+		private void LoadDataAllPage()
+		{
+			LoadDataOfPage(page1.Page1, 1);
+			LoadDataOfPage(page2.Page2, 2);
+			LoadDataOfPage(page3.Page3, 3);
+			LoadDataOfPage(page4.Page4, 4);
+			LoadDataOfPage(page5.Page5, 5);
+		}
 
-            }
-        }
+		private void LoadDataOfPage(Grid xamlpage, byte t)
+		{
+			// Создайте сериализатор XML
+			XmlSerializer serializer = new XmlSerializer(typeof(List<TextBoxData>));
 
-        private void Maximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-                //MaxButImage.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/max.png"));
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-                //MaxButImage.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/win.png"));
-            }
-        }
+			// Десериализуйте коллекцию из файла
+			using (TextReader reader = new StreamReader(LastLoadFile))
+			{
+				textBoxDataList = ((List<TextBoxData>)serializer.Deserialize(reader));
+			}
+			var filteredList = textBoxDataList.Where(data => data.TextBoxName.Contains($"T{t}")).ToList();
+			// Проход по коллекции и установка текста в соответствующие TextBox
+			foreach (var textBoxData in filteredList)
+			{
+				if (textBoxData.Type == typeof(TextBox).Name)
+				{
+					TextBox textBox = (TextBox)xamlpage.FindName(textBoxData.TextBoxName);
 
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
+					if (textBox != null)
+					{
+						textBox.Text = textBoxData.Text;
+					}
+				}
+				else
+				{
+					ComboBox textBox = (ComboBox)xamlpage.FindName(textBoxData.TextBoxName);
 
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            SaveAllPage();
-        }
+					if (textBox != null)
+					{
+						textBox.Text = textBoxData.Text;
+					}
+				}
+			}
+		}
 
-        private void SaveAllPage()
-        {
-            SaveFileDialog folderBrowser = new SaveFileDialog();
-            folderBrowser.Filter = "XML files (*.xml)|*.xml";
-            if (folderBrowser.ShowDialog() == true)
-            {
-                string path = folderBrowser.FileName;
-                textBoxDataList.Clear();
-                SavePage(page1.Page1);
-                SavePage(page2.Page2);
-                SavePage(page3.Page3);
-                SavePage(page4.Page4);
-                SavePage(page5.Page5);
-                SaveFile(path);
-                MessageBox.Show("Файл успешно сохранен!");
-            }
-        }
+		private void Maximize_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.WindowState == WindowState.Maximized)
+			{
+				this.WindowState = WindowState.Normal;
+				//MaxButImage.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/max.png"));
+			}
+			else
+			{
+				this.WindowState = WindowState.Maximized;
+				//MaxButImage.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/win.png"));
+			}
+		}
 
-        private void SaveFile(string path)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<TextBoxData>));
+		private void Minimize_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
 
-            // Сериализуйте коллекцию и сохраните в файл
-            using (TextWriter writer = new StreamWriter(path))
-            {
-                serializer.Serialize(writer, textBoxDataList);
-            }
-        }
+		private void Save_Click(object sender, RoutedEventArgs e)
+		{
+			SaveAllPage();
+		}
 
-        private void SavePage(DependencyObject xamlpage)
-        {
-            foreach (var textBox in FindVisualChildren<TextBox>(xamlpage))
-            {
-                var textBoxData = new TextBoxData
-                {
-                    TextBoxName = textBox.Name,
-                    Text = textBox.Text,
-                    Type = textBox.GetType().Name
-                };
-                textBoxDataList.Add(textBoxData);
-            }
-            foreach (var textBox in FindVisualChildren<ComboBox>(xamlpage))
-            {
-                var textBoxData = new TextBoxData
-                {
-                    TextBoxName = textBox.Name,
-                    Text = textBox.Text,
-                    Type = textBox.GetType().Name
-                };
-                textBoxDataList.Add(textBoxData);
-            }
-        }
+		private void SaveAllPage()
+		{
+			SaveFileDialog folderBrowser = new SaveFileDialog();
+			folderBrowser.Filter = "XML files (*.xml)|*.xml";
+			if (folderBrowser.ShowDialog() == true)
+			{
+				string path = folderBrowser.FileName;
+				textBoxDataList.Clear();
+				SavePage(page1.Page1);
+				SavePage(page2.Page2);
+				SavePage(page3.Page3);
+				SavePage(page4.Page4);
+				SavePage(page5.Page5);
+				SaveFile(path);
+				MessageBox.Show("Файл успешно сохранен!");
+			}
+		}
 
-        private void SetText(DependencyObject xamlpage)
-        {
+		private void SaveFile(string path)
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(List<TextBoxData>));
 
-            foreach (var textBox in FindVisualChildren<TextBox>(xamlpage))
-            {
-                ReplacePlaceholder(document, $"<{textBox.Name}>", textBox.Text);
-            }
-            foreach (var comboBox in FindVisualChildren<ComboBox>(xamlpage))
-            {
-                ReplacePlaceholder(document, $"<{comboBox.Name}>", comboBox.Text);
-            }
-        }
+			// Сериализуйте коллекцию и сохраните в файл
+			using (TextWriter writer = new StreamWriter(path))
+			{
+				serializer.Serialize(writer, textBoxDataList);
+			}
+		}
 
-        private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            App.Current.MainWindow.DragMove();
-        }
-    }
+		private void SavePage(DependencyObject xamlpage)
+		{
+			foreach (var textBox in FindVisualChildren<TextBox>(xamlpage))
+			{
+				var textBoxData = new TextBoxData
+				{
+					TextBoxName = textBox.Name,
+					Text = textBox.Text,
+					Type = textBox.GetType().Name
+				};
+				textBoxDataList.Add(textBoxData);
+			}
+			foreach (var textBox in FindVisualChildren<ComboBox>(xamlpage))
+			{
+				var textBoxData = new TextBoxData
+				{
+					TextBoxName = textBox.Name,
+					Text = textBox.Text,
+					Type = textBox.GetType().Name
+				};
+				textBoxDataList.Add(textBoxData);
+			}
+		}
 
-    [Serializable]
-    public class TextBoxData
-    {
-        public string Text { get; set; }
-        public string TextBoxName { get; set; }
-        public string Type { get; set; }
-    }
+		private void SetText(DependencyObject xamlpage)
+		{
+			foreach (var textBox in FindVisualChildren<TextBox>(xamlpage))
+			{
+				ReplacePlaceholder(document, $"<{textBox.Name}>", textBox.Text);
+			}
+			foreach (var comboBox in FindVisualChildren<ComboBox>(xamlpage))
+			{
+				ReplacePlaceholder(document, $"<{comboBox.Name}>", comboBox.Text);
+			}
+		}
+
+		private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			App.Current.MainWindow.DragMove();
+		}
+	}
+
+	[Serializable]
+	public class TextBoxData
+	{
+		public string Text { get; set; }
+		public string TextBoxName { get; set; }
+		public string Type { get; set; }
+	}
 }
--- Economy/Resources/Windows/LoadWindowVerisonSecond.xaml.cs
+++ Economy/Resources/Windows/LoadWindowVerisonSecond.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,18 +15,56 @@ using System.Windows.Shapes;
 
 namespace Economy.Resources.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для LoadWindowVerisonSecond.xaml
-    /// </summary>
-    public partial class LoadWindowVerisonSecond : Window
-    {
-        public LoadWindowVerisonSecond()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Логика взаимодействия для LoadWindowVerisonSecond.xaml
+	/// </summary>
+	public partial class LoadWindowVerisonSecond : Window
+	{
+		private MainWindow _mainWindow;
 
-        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-        }
-    }
+		public LoadWindowVerisonSecond(MainWindow mainWindow)
+		{
+			InitializeComponent();
+			_mainWindow = mainWindow;
+		}
+
+		private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+		}
+
+		public void IncrementValue(int value)
+		{
+			pbStatus.Value += value;
+		}
+
+		public void ProgressBarStartAsync(SaveFileDialog folderBrowser)
+		{
+			var thr = new Thread(ChangePages);
+			thr.SetApartmentState(ApartmentState.STA);
+			thr.IsBackground = true;
+			thr.Start();
+
+			_mainWindow.document.SaveAs(folderBrowser.FileName);
+			MessageBox.Show("DOCX файл успешно сгенерирован!");
+		}
+
+		private void ChangePages()
+		{
+			Application.Current.Dispatcher.Invoke(() =>
+			{
+				_mainWindow.ChangePage1();
+				IncrementValue(20);
+				_mainWindow.ChangePage2();
+				IncrementValue(20);
+				_mainWindow.ChangePage3();
+				IncrementValue(20);
+				_mainWindow.ChangePage4();
+				IncrementValue(20);
+				_mainWindow.ChangePage5();
+				IncrementValue(20);
+				Close();
+			});
+			System.Windows.Threading.Dispatcher.Run();
+		}
+	}
 }
